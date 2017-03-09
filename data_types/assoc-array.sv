d85be4cc3af1cd8e55cd46269a4c5cbb060d9732
Admin <contact@chipverify.com>
2017-03-08 22:32:52 -0600

    Update assoc-array.sv

--- data_types/assoc-array.sv
+++ data_types/assoc-array.sv
@@ -1,40 +1,50 @@
-// Author   : Admin   (www.chipverify.com)
-// Purpose  : Example of creating a string based associative array
-------------------------------------------------------------------
+// Author   : Admin (www.chipverify.com)
+// Purpose  : Example of how to initialize and use an associative
+//            array. We'll create three different arrays, traverse
+//            and print its contents
+//----------------------------------------------------------------
+
 
 module tb;
-   // Single Dimension associative array
-   int      fruits_l0 [string];
-   string   fruits_l1 [int];   
-
-   initial begin
-      // Assign values to the fruit list
-      fruits_l0 ["apple"] = 4;
-      fruits_l0 ["orange"] = 10;
-      fruits_l0 ["pear"] = 1;
-      foreach (fruits_l0[i]) 
-         $display ("%0d %s", fruits_l0[i], i);
-
-      $display ("------------------------------------");
-      // Assign values to the fruit list
-      fruits_l1 [0] = "Apple";
-      fruits_l1 [1] = "Orange";
-      fruits_l1 [2] = "Guava";
-      foreach (fruits_l1[i]) 
-         $display ("%s %0d", fruits_l1[i], i); 
-   end
+  int     array1  [string];     // integer array with string index
+  int     array2  [int];        // integer array with integer index
+  string  array3  [string];     // string array with string index
+  
+  initial begin
+    array1 = '{ "joey" : 1,
+               "ross" : 2,
+               "rachel" : 10,
+               "monica" : 25};
+               
+    array2 = '{  1 : 22,
+                 6 : 35 };
+     
+    array3 ["Apples"] = "Oranges";
+    array3 ["Pears"] = "44";
+    
+    foreach (array1[name])
+      $display ("array1[%s] = %0d", name, array1[name]);
+      
+    foreach (array2[i])
+      $display ("array2[%0d] = %0d", i, array2[i]);
+      
+    foreach (array3[str1])
+      $display ("array3[%s] = %s", str1, array3[str1]);
+     
+  end
 endmodule
 
 /* Simulation Log:
-------------------
+-------------------
 run -all;
-# KERNEL: 4 apple
-# KERNEL: 10 orange
-# KERNEL: 1 pear
-# KERNEL: ------------------------------------
-# KERNEL: Apple 0
-# KERNEL: Orange 1
-# KERNEL: Guava 2
+# KERNEL: array1[joey] = 1
+# KERNEL: array1[monica] = 25
+# KERNEL: array1[rachel] = 10
+# KERNEL: array1[ross] = 2
+# KERNEL: array2[1] = 22
+# KERNEL: array2[6] = 35
+# KERNEL: array3[Apples] = Oranges
+# KERNEL: array3[Pears] = 44
 # KERNEL: Simulation has finished. There are no more test vectors to simulate.
 
 */
