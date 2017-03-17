0c16c3a24f229739002b668eb9c3e75fb79b4f1a
Admin <contact@chipverify.com>
2017-03-16 22:45:03 -0500

    Update pkg_name_space.sv

--- misc/pkg_name_space.sv
+++ misc/pkg_name_space.sv
@@ -3,20 +3,25 @@
 //              with global namespace
 //-----------------------------------------------------------
 
+// Define a new package to hold an enum type where READ = 0
 package my_pkg;
 	typedef enum bit { READ, WRITE } e_rd_wr;
 endpackage	
 
 import my_pkg::*;
 
+// In the global namespace, create another enum where READ = 1
 typedef enum bit { WRITE, READ } e_wr_rd;
 
+// In this module, create two variables, of each enum type
+// and assign READ to each. See the result with +define+PKG
+// and without 
 module tb;
 	initial begin
         e_wr_rd  	opc1 = READ;
         
 `ifdef PKG
-        e_rd_wr   opc2 = my_pkg::READ;
+        e_rd_wr         opc2 = my_pkg::READ;
 `else
         e_rd_wr  	opc2 = READ;
 `endif
